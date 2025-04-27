37557791ba70e1e0ec68454ee2f53ef7604d44df
sahara <keiichisasahara@gmail.com>
2025-04-27 14:48:31 +0900

    variableAB new

--- variableA.rb
+++ variableA.rb
@@ -1,6 +1,8 @@
 local = "ファイルAで定義したローカル変数です"
 $global = "ファイルAで定義したグローバル変数です"
 
+require_relative "variableB.rb"
+
 puts "ローカル変数: #{local}"
-puts -------------------------
+puts "-------------------------"
 puts "グローバル変数: #{$global}"
